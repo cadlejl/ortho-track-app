a21d9db81a59c2fc208a99ab43722856bd1313d8
Jaime Cadle <cadlejl@yahoo.com>
2017-09-08 10:36:14 -0500

    Add case, facility, surgeon, and system components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SurgeonComponent } from './tracking/surgeon/surgeon.component';
+import { FacilityComponent } from './tracking/facility/facility.component';
+import { SystemComponent } from './tracking/system/system.component';
+import { CaseComponent } from './tracking/case/case.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurgeonComponent,
+    FacilityComponent,
+    SystemComponent,
+    CaseComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tracking/surgeon/surgeon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-surgeon',
+  templateUrl: './surgeon.component.html',
+  styleUrls: ['./surgeon.component.css']
+})
+export class SurgeonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
